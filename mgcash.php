271e6f21c4fd759ac4f2e04c14e9577e5b543de2
CHRISTOPHER MICHAEL-DON ROGERS <ChristopherMDRogers@gmail.com>
2019-04-26 09:51:44 -0700

    Add files via upload

---
+++ mgcash.php
@@ -0,0 +1,5 @@
+<?php
+  //http://mgcash.com/api/?a=direct_offers_feed&key=04fc22ec01212afd&page=1&limit=100
+
+  $mgcash_xml = simplexml_load_file("http://mgcash.com/api/?a=direct_offers_feed&key=04fc22ec01212afd&page=1&limit=10");
+?>
